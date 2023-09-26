ad9e1d8d66731307a8f96e9e9b5f2262392a03bc
asouat <abdel@lizy.be>
2023-09-26 17:33:05 +0200

    refactor: add dark mode on invoice card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
 import { Navbar } from "@/components/navbar";
 import styles from "./page.module.scss";
+import { InvoiceOverviewCard } from "@/components/invoiceOverviewCard";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
+
+      <div className={styles.invoicesWrapper}>
+        <InvoiceOverviewCard
+          invoice={{
+            id: "RT3080",
+            dueDate: "2021-08-19",
+            customer: "Jensen Huang",
+            amount: 1800,
+            status: "paid",
+          }}
+        />
+
+        <InvoiceOverviewCard
+          invoice={{
+            id: "RT3080",
+            dueDate: "2021-08-19",
+            customer: "Jensen Huang",
+            amount: 1800,
+            status: "pending",
+          }}
+        />
+
+        <InvoiceOverviewCard
+          invoice={{
+            id: "RT3080",
+            dueDate: "2021-08-19",
+            customer: "Jensen Huang",
+            amount: 1800,
+            status: "draft",
+          }}
+        />
+      </div>
     </main>
   );
 }
