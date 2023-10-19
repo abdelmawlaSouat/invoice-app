7eab67b7a8df1bccb85da06c9441e5c122c52eb4
asouat <abdel@lizy.be>
2023-10-19 19:48:22 +0200

    feat: create the desktop invoice page

--- src/app/invoice/[id]/invoice.tsx
+++ src/app/invoice/[id]/invoice.tsx
@@ -5,10 +5,42 @@ import { GoBackLink, InvoiceDetailCard, StatusTag } from "@/components";
 import type { Status } from "@/components";
 import { Button, Card, Typography } from "@/design-system/components";
 import { Invoice } from "@/types";
+import classNames from "classnames";
 
 type Props = {
   invoice: Invoice;
 };
+
+const CallToActionGroup = ({ className }: { className?: string }) => (
+  <div className={classNames(styles.ctasWrapper, className)}>
+    <Button
+      className={styles.editBtn}
+      onClick={() => {
+        alert("Edit");
+      }}
+    >
+      Edit
+    </Button>
+
+    <Button
+      className={styles.deleteBtn}
+      onClick={() => {
+        alert("Delete");
+      }}
+    >
+      Delete
+    </Button>
+
+    <Button
+      className={styles.markAsPaidBtn}
+      onClick={() => {
+        alert("Mark as Paid");
+      }}
+    >
+      Mark as Paid
+    </Button>
+  </div>
+);
 
 export default function Invoice({ invoice }: Props) {
   return (
@@ -16,46 +48,29 @@ export default function Invoice({ invoice }: Props) {
       <div className={styles.content}>
         <GoBackLink />
 
-        <Card className={styles.statusCard}>
-          <Typography
-            className={styles.secondaryText}
-            variant="body"
-            tag="span"
-          >
-            Status
-          </Typography>
+        <Card className={styles.statusAndCtasWrapper}>
+          <div className={styles.statusWrapper}>
+            <Typography
+              className={styles.secondaryText}
+              variant="body"
+              tag="span"
+            >
+              Status
+            </Typography>
 
-          <StatusTag status={invoice.status as Status} />
+            <StatusTag status={invoice.status as Status} />
+          </div>
+
+          <div className={styles.ctaGroupOnDesktop}>
+            <CallToActionGroup className={styles.CtaGroup} />
+          </div>
         </Card>
 
         <InvoiceDetailCard invoice={invoice} />
       </div>
 
-      <Card className={styles.ctasWrapper}>
-        <Button
-          className={styles.editBtn}
-          onClick={() => {
-            alert("Edit");
-          }}
-        >
-          Edit
-        </Button>
-        <Button
-          className={styles.deleteBtn}
-          onClick={() => {
-            alert("Delete");
-          }}
-        >
-          Delete
-        </Button>
-        <Button
-          className={styles.markAsPaidBtn}
-          onClick={() => {
-            alert("Mark as Paid");
-          }}
-        >
-          Mark as Paid
-        </Button>
+      <Card className={styles.ctaGroupOnMobile}>
+        <CallToActionGroup />
       </Card>
     </main>
   );
---
+++ src/components/invoiceDetailCard/MobileItemsCard.tsx
@@ -0,0 +1,54 @@
+import { Card, Typography } from "@/design-system/components";
+import styles from "./MobileItemsCard.module.scss";
+
+export type Item = {
+  name: string;
+  quantity: number;
+  price: number;
+  total: number;
+};
+
+export const MobileItemsCard = ({
+  items,
+  total,
+}: {
+  items: Item[];
+  total: number;
+}) => {
+  return (
+    <Card className={styles.itemsCard}>
+      {items.map((item) => (
+        <div className={styles.itemWrapper} key={item.name}>
+          <div>
+            <Typography variant="headingS">{item.name}</Typography>
+
+            <Typography className={styles.quantity} variant="body">
+              {`${item.quantity} x ${item.price.toLocaleString("en-BE", {
+                style: "currency",
+                currency: "EUR",
+              })}`}
+            </Typography>
+          </div>
+
+          <Typography variant="headingS">
+            {item.total.toLocaleString("en-BE", {
+              style: "currency",
+              currency: "EUR",
+            })}
+          </Typography>
+        </div>
+      ))}
+
+      <div className={styles.itemsFooter}>
+        <Typography variant="headingS"> Total</Typography>
+
+        <Typography variant="headingM">
+          {total.toLocaleString("en-BE", {
+            style: "currency",
+            currency: "EUR",
+          })}
+        </Typography>
+      </div>
+    </Card>
+  );
+};
